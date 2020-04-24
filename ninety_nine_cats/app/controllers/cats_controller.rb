d73ad959e1bc3cf5d23bfd96eb150dcc0281f11e
Jae-Son Song <jaesonsong@gmail.com>
2020-04-24 17:16:17 -0400

    Start edit form

--- ninety_nine_cats/app/controllers/cats_controller.rb
+++ ninety_nine_cats/app/controllers/cats_controller.rb
@@ -25,6 +25,21 @@ class CatsController < ApplicationController
     end
   end
 
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+
+    if @cat.update_attributes(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
+  end
+
   def new
     @cat = Cat.new
     render :new
